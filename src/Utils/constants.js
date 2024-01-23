51dee66021747d51a156b06ccb376467c85dedf4
VKoder <vivekkhule204@gmail.com>
2024-01-23 17:41:49 +0530

    cors link

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -7,7 +7,7 @@ export const REST_MENU_API =
 export const REST_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.447523597754362&lng=73.85565485805273&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-export const CORS_API = "https://thingproxy.freeboard.io/fetch/";
+export const CORS_API = "https://corsproxy.org/?";
 
 export const ITEM_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.447523597754362&lng=73.85565485805273&collection=";
