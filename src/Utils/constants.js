99184d0f15e3f534ce98d6779f1dbd268c0862eb
VKoder <vivekkhule204@gmail.com>
2024-01-23 18:36:31 +0530

    solving cors issue

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -2,12 +2,12 @@ export const CARD_IMG_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/";
 
 export const REST_MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.4529322&lng=73.86523799999999&restaurantId=";
+  "menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.4529322&lng=73.86523799999999&restaurantId=";
 
 export const REST_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.447523597754362&lng=73.85565485805273&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "restaurants/list/v5?lat=18.447523597754362&lng=73.85565485805273&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-export const CORS_API = "https://corsproxy.org/?";
+export const CORS_API = "https://strange-fawn-gabardine.cyclic.app/api/proxy/swiggy/dapi/";
 
 export const ITEM_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.447523597754362&lng=73.85565485805273&collection=";
